c4a4ab5bb1d9669558b28ae7b18d7d734c2b9bfe
jonas_lindemann <jonas_lindemann@26702652-3222-4c33-a8c0-cd4b7a740f10>
2008-01-20 22:54:19 +0000

    More work on new solver.

--- src/opt_solver/Force.h
+++ src/opt_solver/Force.h
@@ -2,19 +2,15 @@
 #define _CForce_H_
 
 #include "Vec3d.h"
-#include "Node3d.h"
 
 SmartPointer(CForce);
 
 class CForce : public CVec3d {
 private:
-	CNode3dPtr m_node;
+
 public:
 	CForce ();
 	virtual ~CForce ();
-
-	void setNode(CNode3d* node);
-	CNode3d* getNode();
 
 	ClassInfo("CForce", CVec3d);
 };
